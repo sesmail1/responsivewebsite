ddcd5258895d84e99de29fd596d4ac9e4c98622e
Sameer Dewan <sesmail1@students.kennesaw.edu>
2018-07-10 09:06:54 -0400

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+function windowSizer() {
+        $('#aboutcard, #summary').click(() => {
+        $('#summary').toggle();
+    });
+};
+
+function windowSizer2() {
+    $
+}
+
+
+function disabledSizer(valid) {
+    if (valid == true) {
+        $('#aboutcard, #summary').off('click');
+    }
+    if (valid == false) {
+        windowSizer();
+    }
+};
+
+$(window).resize(function() {
+    // This will fire each time the window is resized:
+    if($(window).width() < 801) {
+        disabledSizer(false);
+    } else {
+            disabledSizer(true);
+            $('#summary').show();
+    }
+}).resize();
